228fce2775bb15f2ea3565110bf977c5442ea3dd
thecalmdevil <kampvid@gmail.com>
2021-06-14 20:21:02 -0400

    Putting Public Key into commerce.js

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce(process.env.pk_289032054d4ecc4703165f1a6356ba4d55b061323232a, true);
