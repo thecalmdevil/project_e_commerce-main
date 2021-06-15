3a0402b156e4961ff3e2134eb63a202a0e1eba14
thecalmdevil <kampvid@gmail.com>
2021-06-14 20:47:47 -0400

    Moved Commerce,js key from commerce.js to .env

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.pk_289032054d4ecc4703165f1a6356ba4d55b061323232a, true);
+export const commerce = new Commerce(process.env.true);
